8743a46bed89da78a1a1c0061116955271845a45
Everspace <7560635+Everspace@users.noreply.github.com>
2022-03-24 00:00:26 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import random from "lodash/random"
+import range from "lodash/range"
+import { useState } from "react"
+import './App.css'
+
+const beingTable = [
+  "⚓🍎💰🍺🔔🐦💣🦴📕🧹🐛🍭🥕🐱🧪♟🍗🎞☁☕🧭🤠👑🐶🚪🥚🌲👁💎🐟🚩🔥🍴⚙",
+  "🎁🥛🎸💨👻🔨👷🎧♥⌛🏠🧠✋💀🦷🍦🔑🛡🍁🍋💡⚡💧🧲🔍🧤💵🌙🎵🔒🖌✂🎲📎✈",
+  "📜🐾✏🌶🧩☢🕶📏⚖🔧👣🦐🍞🍕❄⚽🧦🕷🥄⭐💼☀💉📞🎭🌡🌪🚮🏆👕☂🌊🧀💪🍷",
+].flatMap(s=> Array.from(s))
+
+const doingTable = Array.from("🚫🗨✔🎯➗⬇=!…⏩💠💥📈♾±≠⏸%📊▶❓♻⏪♀♂∛🔀🔗⬆💢🛑🔄⚛")
+
+const pick = (table: string[]) => {
+  return table.at(random(1, table.length) -1) as string
+}
+
+const makeBeing = () => range(5).map(_ => pick(beingTable))
+const makeDoing = () => range(3).map(_ => pick(doingTable) + pick(beingTable))
 
 function App() {
+  const [being, setBeing] = useState(makeBeing())
+  const [doing, setDoing] = useState(makeDoing())
+  const newCharacter = () => {
+    setBeing(makeBeing())
+    setDoing(makeDoing())
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-logo">🔄🐦</div>
+        <ul>
+          <li>{being.join("")}</li>
+          {doing.map((arr, i) => <li key={i}>{arr}</li>)}
+        </ul>
+        <button onClick={newCharacter}>
+        🔄❓
+        </button>
+        <a style={{marginTop: "2em"}} href="https://penguinking.com/rotate-bird/">©🐧👑</a>
       </header>
     </div>
   );
